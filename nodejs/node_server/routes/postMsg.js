8a59c04e22cce5ae7538b8fea4cd64098eb378ff
knightingal <yujianming1985@126.com>
2015-12-04 00:12:10 +0800

    fix bug of sometimes the error of download lots of image add interface for navy  Changes to be committed: 	modified:   nodejs/node_server/routes/postMsg.js

--- nodejs/node_server/routes/postMsg.js
+++ nodejs/node_server/routes/postMsg.js
@@ -25,6 +25,9 @@ var gSuccCount = 0;
 
 function getHttpReqCallback(fileName, dirName) {
     return function (res) {
+      // console.log('STATUS: ' + res.statusCode);
+      // console.log('HEADERS: ' + JSON.stringify(res.headers));
+        var contentLength = parseInt(res.headers['content-length']);
         var fileBuff = [];
         res.on('data', (function(fileName) {
             return function (chunk) {
@@ -32,9 +35,14 @@ function getHttpReqCallback(fileName, dirName) {
                 fileBuff.push(buffer);
             };
         })(fileName));
-        res.on('end', (function(fileName) {
+        res.on('end', (function(fileName, contentLength) {
             return function() {
                 var totalBuff = Buffer.concat(fileBuff);
+                console.log("bufferLenght = " + totalBuff.length + ", this contentLength = " + contentLength);
+                if (totalBuff.length < contentLength) {
+                  console.log(fileName + " download error!");
+                  return;
+                }
                 fs.appendFile(dirName + "/" + fileName, totalBuff, function(err){});
                 gSuccCount += 1;
                 console.log("(" + gSuccCount + "/" + gImgCount + ")" + fileName + " download succ!");
@@ -45,7 +53,7 @@ function getHttpReqCallback(fileName, dirName) {
                     router.initCb();
                 }
             };
-        })(fileName))
+        })(fileName, contentLength))
     };
 }
 
@@ -57,19 +65,43 @@ function startDownload(imgSrc, dirName) {
         path: urlObj.path,
         headers: new ReqHeadersTemp()
     };
-    
+
     var req = http.request(options, getHttpReqCallback(fileName, dirName));
     req.setTimeout(60 * 1000, function() {
         console.log(imgSrc + 'timeout, try again...');
         req.abort();
-        startDownload(imgSrc, dirName); 
+        startDownload(imgSrc, dirName);
     });
     req.on('error', function(e) {
-        startDownload(imgSrc, dirName);             
+        startDownload(imgSrc, dirName);
     });
-    
+
     req.end();
 }
+
+function downloadFor20(imgSrcArray, dirName) {
+  for (var i = 0; i < imgSrcArray.length; i++) {
+      var imgSrc = imgSrcArray[i].imrSrc;
+      startDownload(imgSrc, dirName)
+  }
+}
+
+router.post('/donwLoadNavy', function(req, res) {
+  console.log(req.body);
+  res.send("ok");
+  gImgCount += req.body.imgArray.length;
+  var dirName = RootDirString + req.body.title;
+
+  fs.mkdir(dirName, function() {
+    console.log(req.body.imgArray);
+    var imgSrcArray = req.body.imgArray;
+    // downloadFor20(imgSrcArray.slice(0, 20), dirName)
+    for (var i = 0; i < imgSrcArray.length; i++) {
+      var imgSrc = imgSrcArray[i].imrSrc;
+      startDownload(imgSrc, dirName);
+    }
+  });
+});
 
 //TODO: so many anonymous function, and callback hell!!!
 router.post('/', function(req, res) {
@@ -77,20 +109,21 @@ router.post('/', function(req, res) {
     res.send('ok');
     gImgCount += req.body.imgSrcArray.length;
     var dirName = RootDirString + req.body.title;
-    
-    
+
+
     fs.mkdir(dirName, function() {
-        
+
         console.log(req.body.imgSrcArray);
         console.log(req.body.href);
 
         var imgSrcArray = req.body.imgSrcArray;
+
         // var pageHref = req.body[j].href;
         for (var i = 0; i < imgSrcArray.length; i++) {
             var imgSrc = imgSrcArray[i];
             startDownload(imgSrc, dirName)
         }
-        
+
     });
 
 });
